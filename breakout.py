0a20b62853ff95f95c23fd738e4fb32000135abc
atokusumi <31392375+atokusumi@users.noreply.github.com>
2017-11-08 07:38:04 -0500

    add shaking

--- breakout.py
+++ breakout.py
@@ -2,7 +2,8 @@
 
 import sys, pygame, random, time, math, glob
 assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
-
+from random import randint
+from itertools import repeat
 
 NORTH = math.pi
 NORTHEAST = 5/4*math.pi
@@ -271,11 +272,11 @@ screen_size = (1024,768)
 FPS = 60
 points_position = (10,10)
 lives_position = (1000,10)
-display_color = (255,255,255)
+display_color = (111,111,111)
 
 paddle_pos = (0,700)
 paddle_size = (80,15)
-paddle_color = (255,255,255)
+paddle_color = (255,0,0)
 paddle_max_speed = 60
 
 num_blocks = 10
@@ -361,6 +362,72 @@ def main():
 		game.draw_lives(screen,lives)
 		game.draw_points(screen,points)
 		pygame.display.flip()
+
+pygame.init()
+org_screen = pygame.display.set_mode((1024,768))
+screen = org_screen.copy()
+screen_rect = screen.get_rect()
+player = pygame.Rect(180, 180, 20, 20)
+
+def get_rock():
+	return pygame.Rect(randint(0, 340), 0, 60, 60)
+
+falling = get_rock()
+clock = pygame.time.Clock()
+
+# 'offset' will be our generator that produces the offset
+# in the beginning, we start with a generator that 
+# yields (0, 0) forever
+offset = repeat((0, 0))
+
+# this function creates our shake-generator
+# it "moves" the screen to the left and right
+# three times by yielding (-5, 0), (-10, 0),
+# ... (-20, 0), (-15, 0) ... (20, 0) three times,
+# then keeps yieling (0, 0)
+def shake():
+	s = -1
+	for _ in range(0, 3):
+		for x in range(0, 20, 5):
+			yield (x*s, 0)
+		for x in range(20, 0, 5):
+			yield (x*s, 0)
+		s *= -1
+	while True:
+		yield (0, 0)
+
+while True:
+	pygame.event.pump()
+
+	if pygame.event.get(pygame.QUIT): sys.exit()
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_w]: player.move_ip(0, -2)
+	if keys[pygame.K_a]: player.move_ip(-2, 0)
+	if keys[pygame.K_s]: player.move_ip(0, 2)
+	if keys[pygame.K_d]: player.move_ip(2, 0)
+	player.clamp_ip(screen_rect) 
+
+	falling.move_ip(0, 4)
+	org_screen.fill((111, 111, 111))
+	screen.fill((111,111,111))
+	pygame.draw.rect(screen, (0,0,0), player)
+	pygame.draw.rect(screen, (255,0,0), falling)
+
+	if player.colliderect(falling):
+		# if the player is hit by the rock,
+		# we create a new shake-generator
+		offset = shake()
+		falling = get_rock()
+
+	if not screen_rect.contains(falling):
+		falling = get_rock()
+
+	clock.tick(100)
+	# here we draw our temporary surface to the
+	# screen using the offsets created by the 
+	# generators.
+	org_screen.blit(screen, next(offset))
+	pygame.display.flip()
 
 if __name__ == '__main__':
 	main()
